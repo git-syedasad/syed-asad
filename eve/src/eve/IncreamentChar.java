6ffd823cb61ef0b34b6f9c8c53cdcc5c9d63f93b
SyedAsad <@xyz.com>
2022-02-07 16:16:35 +0400

    Feb3

---
+++ eve/src/eve/IncreamentChar.java
@@ -0,0 +1,11 @@
+package eve;
+
+public class IncreamentChar {
+public static void main(String args[])
+{
+	char ch='x';
+	System.out.println("THE CHARACTER IS"+ch);
+	ch++;
+	System.out.println("NOW THE CHARACTER IS"+ch);
+}
+}
