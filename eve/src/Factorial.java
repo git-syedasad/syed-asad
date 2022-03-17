3342decfa47b7076d2f4f8a83ab06fafce0a5d28
syed asad <syed asad@LAPTOP-S30GC83S>
2022-03-17 16:09:56 +0400

    17th mar

---
+++ eve/src/Factorial.java
@@ -0,0 +1,14 @@
+
+public class Factorial {
+	int fact(int n) {
+		int result;
+		if(n==1) return 1;
+		result=fact(n-1)*n;
+		return result;
+	}
+	public static void main(String[] args) {
+		Factorial f=new Factorial();
+		System.out.println(" "+f.fact(3));
+		System.out.println(" "+f.fact(5));
+	}
+}
